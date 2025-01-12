013f98cd34e1d67e3ce07b0bbdbdb4374a4874a8
Sergey <gonsoronss@icloud.com>
2025-01-12 13:37:37 +0300

    12.01

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,9 +1,6 @@
 package koschei.config;
 
-import koschei.models.Egg6;
-import koschei.models.Island2;
-import koschei.models.Needle7;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,4 +14,8 @@ public class AppConfig {
         return new Island2(wood);
     }
 
+    @Bean
+    public static Duck5 getDuck(Egg6 egg) {
+        return new Duck5(egg);
+    }
 }
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,11 +1,9 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class Duck5 {
 
-    private Egg6 egg6;
+    private final Egg6 egg6;
 
     public Duck5(Egg6 egg6) {
         this.egg6 = egg6;
