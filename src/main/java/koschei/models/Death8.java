466ff15fe2dd94e92fabc77bdb4ec7d6982b1598
Sergey <gonsoronss@icloud.com>
2025-01-12 13:29:56 +0300

    12.01

--- src/main/java/koschei/models/Death8.java
+++ src/main/java/koschei/models/Death8.java
@@ -3,12 +3,10 @@ package koschei.models;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Needle7 {
-
-    private Death8 death8;
+public class Death8 {
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return "";
     }
 }
